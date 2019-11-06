7f1853adc14b08a349f11e03cf06e2f76b261d29
lucassoller2000 <lucassoller20000@gmail.com>
2019-11-05 23:21:46 -0300

    enviando exemplo de upload de imagens no front

--- BrainHelp Web/src/components/Telas/Teste.jsx
+++ BrainHelp Web/src/components/Telas/Teste.jsx
@@ -1,37 +1,68 @@
 import React from 'react'
-import Alert from '../generic/Alert/Alert'
-import CadastroService from '../../Services/CadastroService'
-import LoginService from '../../Services/LoginService'
 import './Cadastro.css'
-import {Redirect} from 'react-router-dom'
-import $ from 'jquery'
 import './Teste.css'
-
+import RegisterService from '../../Services/RegisterService'
 
 
 export default class Teste extends React.Component{
     constructor(){
         super()
         this.state = {
-
+            file: ""
         }
+        this.enviar = this.enviar.bind(this)
+        this.handdleChange = this.handdleChange.bind(this)
     
     }
 
+    handdleChange(event) {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleFiles(e) {
+        this.setState({
+           file: e.target.files[0]
+        })
+        this.setarImagem(e)
+    }
+
+    setarImagem(e){
+        var reader = new FileReader();     
+        reader.onload = function(i) {
+            document.getElementById("teste-imagem").setAttribute("src", reader.result )
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+
+
+
+    enviar(){ 
+        var formData = new FormData();
+        formData.append("file", this.state.file)
+        formData.append("email", "appbrainhelp@gmail.com") 
+        RegisterService.editarFoto(formData)
+    }
 
     render(){
-
         return(<div className="teste-container">
-        
                     <input 
                         type="file"
                         placeholder="Nome"
                         name="nome"
-                        value={this.state.nome}
-                        onChange={this.handdleChange}
                         accept="image/png, image/jpeg"
-                        className="info-input"          
+                        className="info-input"   
+                        id="arquivo"
+                        onChange={(e)=>this.handleFiles(e)}       
                     />
+
+                    <input type="submit" className="teste-enviar" onClick={this.enviar}/>
+
+                    <img id="teste-imagem" src=""/>
 
                                
         </div>)
